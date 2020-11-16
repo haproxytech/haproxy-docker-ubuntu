59545f6646d6f42cd4ff0c8e72878b228f51a44e
Dinko Korunic <dkorunic@haproxy.com>
2020-11-16 18:24:54 +0000

    Automated commit triggered by 2.3.1 release(s)

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 DOCKER_TAG="haproxytech/haproxy-ubuntu"
 HAPROXY_GITHUB_URL="https://github.com/haproxytech/haproxy-docker-ubuntu/blob/master"
-HAPROXY_BRANCHES="1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3"
-HAPROXY_CURRENT_BRANCH="2.2"
+HAPROXY_BRANCHES="1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4"
+HAPROXY_CURRENT_BRANCH="2.3"
 PUSH="no"
 HAPROXY_UPDATED=""
 
