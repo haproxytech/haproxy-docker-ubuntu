5d3bbf4ea390c105b2dddf1c3bb846a9509c9f42
Dinko Korunic <dkorunic@haproxy.com>
2023-06-02 09:43:53 +0000

    Change 2.8 into full release and add 2.9 branch

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 DOCKER_TAG="haproxytech/haproxy-ubuntu"
 HAPROXY_GITHUB_URL="https://github.com/haproxytech/haproxy-docker-ubuntu/blob/main"
-HAPROXY_BRANCHES="2.0 2.2 2.4 2.5 2.6 2.7 2.8"
-HAPROXY_CURRENT_BRANCH="2.7"
+HAPROXY_BRANCHES="2.0 2.2 2.4 2.5 2.6 2.7 2.8 2.9"
+HAPROXY_CURRENT_BRANCH="2.8"
 PUSH="no"
 HAPROXY_UPDATED=""
 
